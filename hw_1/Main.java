823c3bc9bf1db179a1d3d1552af63e5a618b8657
shachafhaviv <shachaf.haviv@gmail.com>
2022-04-02 17:18:11 +0300

    add_files

---
+++ hw_1/Main.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner scanner;  // Note: Do not change this line.
+
+    public static void theStudentsGame() {
+        // TODO: Add your code here.
+    }
+
+    public static void main(String[] args) throws IOException {
+        String path = args[0];
+        scanner = new Scanner(new File(path));
+        int numberOfGames = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i = 1; i <= numberOfGames; i++) {
+            System.out.println("Game number " + i + " starts.");
+            theStudentsGame();
+            System.out.println("Game number " + i + " ended.");
+            System.out.println("-----------------------------------------------");
+        }
+        System.out.println("All games are ended.");
+    }
+}
